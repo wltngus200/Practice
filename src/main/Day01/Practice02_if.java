05c561c0414d7939583c2288c0f2b062fa62aae3
user <wltngus200@gmail.com>
2024-03-17 00:10:22 +0900

    Nothing

---
+++ src/main/Day01/Practice02_if.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class Practice02_if {//같은 이름의 클래스 정의
+    public static void main(String[] args){
+        //메인 메서드 지정(프로그램 구동할 수 있게)
+        Scanner wtf=new Scanner(System.in);
+        //객체 선언 및 초기화 키보드로 입력 가능
+        /*
+        System.out.print("정수값:");
+        int n=wtf.nextInt();
+        //다음에 오는 값은 인트이고 n에 대입됨
+        //키보드로 입력을 가능하게 하는 wtf이라는 명령어를 불러냈다?
+
+        boolean x;
+        int i;
+
+        //for(i=0,x==true,i++);
+        //양수가 나올때까지 계속, 근데 이건 break에 가까운가
+            if (n > 0)
+                System.out.println("이 값은 양수입니다.");
+            else if (n == 0)
+                System.out.println("0입니다");
+            else
+                System.out.println("이 값은 음수입니다.");
+         // 중괄호가 없어도 되네?
+        */
+
+        /*
+        int a=4;
+        if(a>5 || a%2==0)
+            System.out.println("이 기호가 맞습니다");
+        // ||(or)기호는 원 기호 위의 기호
+        */
+
+        /*
+        //3가지 수를 비교하는 if문
+        System.out.print("a:");
+        int a=wtf.nextInt();
+        System.out.print("b:");
+        int b=wtf.nextInt();
+        System.out.print("c:");
+        int c=wtf.nextInt();
+        int max=a; //a가 가장 크다고 가정
+        if(a<b){ //b가 a보다 크면 최대값을 b로
+            max=b;} //아니라면 아무 일도 안 일어남 즉, a가 max인 채
+        if (a<c){
+            max=c;}
+        System.out.print("가장 큰 수는"+max+"입니다.");
+        */
+        }
+
+}
---
+++ src/main/Day01/Practice03_switch.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class Practice03_switch {
+    public static void main(String[] args){
+        Scanner Rsp=new Scanner(System.in);
+        /*
+        System.out.print("가위바위보(가위=0,바위=1,보=2)");
+        int vic=Rsp.nextInt();
+        switch(vic){
+            case 0:
+                System.out.print("가위");
+                break;
+            case 1:
+                System.out.print("바위");
+                break;
+            case 2:
+                System.out.print("보");
+                break;
+        //else if문이 반복되는 것을 방지 2강+p.146
+        }
+         */
+    System.out.print("정수를 입력하세요.:");
+    int num=Rsp.nextInt();
+    switch(num){
+        case 0:
+            System.out.print("A");
+            System.out.print("B");
+            //0이 들어오면 2가지 실행
+            break;
+        case 2:
+            System.out.print("C");
+            //C를 실행하고 브레이크가 없어 아래로 진행
+        case 800:
+            System.out.print("What");
+            //이건 내 개인적 호기심 숫자 크키는 상관 없다.
+        case 5:
+            System.out.print("D");
+            break;
+        case 6:
+            //역시 아무것도 없어 실행X
+            // 브레이크가 없어 아래로 진행
+        case 7:
+            System.out.print("E");
+            break;
+            //아래가 디폴트라도 브레이크가 없으면 실행됨
+        default:
+            System.out.print("F");
+            break;
+            //그 외의 어떤 값을 주어도 디폴트값
+    }
+    }
+}
