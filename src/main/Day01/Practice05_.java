ab2fd4cf6f43b6b57963447682e58f85b71f3724
user <wltngus200@gmail.com>
2024-03-18 00:01:42 +0900

    go to bed

---
+++ src/main/Day01/Practice05_.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+public class Practice05_ {
+    public static void main(String[] asgs){
+        //배열 여러개의 값을 한꺼번에 처리하기 위해 필요
+        /*
+        Scanner scan= new Scanner(System.in);
+        int sum=0;
+        System.out.println("5명의 점수를 입력하시오.");
+
+        System.out.print("1번 점수:");
+        int lee = scan.nextInt();
+        sum+=lee;
+
+        System.out.print("2번 점수:");
+        int kim = scan.nextInt();
+        sum+=kim;
+
+        System.out.print("3번 점수:");
+        int park = scan.nextInt();
+        sum+=park;
+
+        System.out.print("4번 점수:");
+        int hong = scan.nextInt();
+        sum+=hong;
+
+        System.out.print("5번 점수:");
+        int jung =scan.nextInt();
+        sum+=jung;
+
+        System.out.println("합계는"+sum+"점입니다.");
+        System.out.println("평균은"+(double)sum/5+"점입니다.");//실수 doubble
+        */
+
+        /*int[] a=new int[5];//5개 0,1,2,3,4
+        a[1]=37;
+        a[2]=51;
+        a[4]=a[1]*2;
+
+        System.out.println("a["+0+"]="+a[0]);//초기값
+        System.out.println("a["+1+"]="+a[1]);
+        System.out.println("a["+2+"]="+a[2]);
+        System.out.println("a["+3+"]="+a[3]);//초기값
+        System.out.println("a["+4+"]="+a[4]);
+
+        for(int i=0; i<5; i++){
+            System.out.println("a["+i+"]="+a[i]);} //위에 거 한번에
+        */
+
+        int[] arw=new int[5];
+        for(int i=0;i<arw.length;i++){
+            System.out.print("비어서 그런가. 진짜네");
+            //4강 9분 언저리~
+        }
+
+
+
+
+
+
+    }
+
+}
