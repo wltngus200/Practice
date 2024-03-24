8850194bef53ff54e3e51ebf4ae9b52b4691c3f0
user <wltngus200@gmail.com>
2024-03-25 00:13:33 +0900

    go to bed

--- src/main/Day01/Practice09_classvar_classmath.java
+++ src/main/Day01/Practice09_classvar_classmath.java
@@ -3,7 +3,21 @@ import java.util.Scanner;
 public class Practice09_classvar_classmath {
     public static void main(String[] args){
         Scanner scan=new Scanner(System.in);
-        if
+    }
+}
+
+class Account{
+    private static int counter=0; //클래스변수
+    //Static=정적인 -> 정적인 필드
+    private String name;
+    private String no;
+    private long balance;
+    private int id;
+    //네가지 모두 비정적인 필드 //인스턴스 변수?
+
+
+    //Account의 생성자
+    public Account(String n,String num, long z){
 
     }
 }
