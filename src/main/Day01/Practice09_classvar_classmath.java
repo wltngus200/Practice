0e65ea868abc1b4e69404f7e2372639dfcb3754b
user <wltngus200@gmail.com>
2024-03-24 20:48:38 +0900

    go to bed

---
+++ src/main/Day01/Practice09_classvar_classmath.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Practice09_classvar_classmath {
+    public static void main(String[] args){
+        Scanner scan=new Scanner(System.in);
+        if
+
+    }
+}
